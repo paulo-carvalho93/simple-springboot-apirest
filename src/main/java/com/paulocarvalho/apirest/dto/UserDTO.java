9359857d3b3251ed214dcb73bc6bd0c4036732ad
PAULO CARVALHO <paulo.carvalho@pontosmultiplus.com.br>
2020-04-29 14:56:24 -0300

    Padrao DTO para recuperar usuários com função Lambda

---
+++ src/main/java/com/paulocarvalho/apirest/dto/UserDTO.java
@@ -0,0 +1,49 @@
+package com.paulocarvalho.apirest.dto;
+
+import java.io.Serializable;
+
+import com.paulocarvalho.apirest.domain.User;
+
+public class UserDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	private String email;
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public UserDTO() {
+		
+	}
+	
+	public UserDTO(User obj) {
+		id = obj.getId();
+		name = obj.getName();
+		email = obj.getEmail();
+	}
+	
+}
