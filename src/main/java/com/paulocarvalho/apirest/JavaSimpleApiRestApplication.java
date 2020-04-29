209f0a59c36eb00e94cc4b41aa535dcb6aa818b7
PAULO CARVALHO <paulo.carvalho@pontosmultiplus.com.br>
2020-04-29 13:56:07 -0300

    Initial commit

---
+++ src/main/java/com/paulocarvalho/apirest/JavaSimpleApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.paulocarvalho.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSimpleApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSimpleApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulocarvalho/apirest/JavaSimpleApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulocarvalho.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSimpleApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
