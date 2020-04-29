4f58e23d89fc20550b2ea01d1c75c9b022ddbe13
PAULO CARVALHO <paulo.carvalho@pontosmultiplus.com.br>
2020-04-29 15:47:19 -0300

    Inserção do método POST

--- src/main/java/com/paulocarvalho/apirest/services/UserService.java
+++ src/main/java/com/paulocarvalho/apirest/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.paulocarvalho.apirest.domain.User;
+import com.paulocarvalho.apirest.dto.UserDTO;
 import com.paulocarvalho.apirest.repository.UserRepository;
 import com.paulocarvalho.apirest.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
